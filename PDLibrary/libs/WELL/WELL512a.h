8da1c33e324a64089c614ec6abaf87cc29efc685
pasteldew <PastelDew@gmail.com>
2019-09-07 14:50:06 +0900

    Include WELL randomizing algorithm

---
+++ PDLibrary/libs/WELL/WELL512a.h
@@ -0,0 +1,13 @@
+/* ***************************************************************************** */
+/* Copyright:      Francois Panneton and Pierre L'Ecuyer, University of Montreal */
+/*                 Makoto Matsumoto, Hiroshima University                        */
+/* Notice:         This code can be used freely for personal, academic,          */
+/*                 or non-commercial purposes. For commercial purposes,          */
+/*                 please contact P. L'Ecuyer at: lecuyer@iro.UMontreal.ca       */
+/* ***************************************************************************** */
+
+#include "../../PDCore.h"
+
+PDLDLL_API void InitWELLRNG512a (unsigned int *init);
+PDLDLL_API double WELLRNG512a (void);
+
